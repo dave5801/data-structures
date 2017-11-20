0418f9d1873d014f0b8b377cfd9dae3afedb3f1b
David Franklin <dave5801@gmail.com>
2017-11-19 18:20:23 -0800

    add neighbors and get weight to vertex class

--- test_w_graph.py
+++ test_w_graph.py
@@ -8,8 +8,54 @@ def test_vertex_exists_no_args():
     assert test_vertex
 
 
+def test_vertex_has_single_neighbor():
+    """Test creation of new vertex."""
+    from weighted_graph import Vertex
+    test_vertex = Vertex({'A': {'B': 5}})
+
+    assert test_vertex.get_vertex() == 'A'
+    test_vertex.update_neighbors()
+
+    assert test_vertex.neighbors == ['B']
+
+
+def test_vertex_get_weight_from_single_neighbor():
+    """Test getting a single weight."""
+    from weighted_graph import Vertex
+    test_vertex = Vertex({'A': {'B': 5}})
+
+    test_vertex.update_neighbors()
+
+    test_neighbor_list = test_vertex.neighbors
+    test_neighbor = test_neighbor_list[0]
+
+    assert test_vertex.get_weight(test_neighbor) == 5
+
+
+def test_vertex_has_multiple_neighbors():
+    """Test if Vertex has multiple neighbors."""
+    from weighted_graph import Vertex
+    test_vertex = Vertex({'A': {'B': 5, 'C': 10, 'D': 6}})
+    test_vertex.update_neighbors()
+
+    assert test_vertex.neighbors == ['B', 'C', 'D']
+
+
+def test_vertex_get_weight_one_of_many_neighbors():
+    """Test if single weight returned from multi neighbors."""
+    from weighted_graph import Vertex
+
+    test_vertex = Vertex({'A': {'B': 5, 'C': 10, 'D': 6}})
+    test_vertex.update_neighbors()
+
+    test_neighbor_list = test_vertex.neighbors
+    test_neighbor = test_neighbor_list[1]
+    assert test_vertex.get_weight(test_neighbor) == 10
+
+
+'''
 def test_vertex_has_a_vertex_with_single_neighbor():
-    """test vertex for single neighbor"""
+    """test vertex for single neighbor."""
     from weighted_graph import Vertex
     test_vertex = Vertex('A', ['B'], {'B': 5})
     test_data = 'A'
@@ -21,12 +67,25 @@ def test_vertex_has_a_vertex_with_single_neighbor():
 
 
 def test_vertex_multiple_neigbors():
-    """test vertex multiple neighbors"""
+    """test vertex multiple neighbors."""
     from weighted_graph import Vertex
     test_vertex = Vertex('A', ['B', 'C', 'D'], {'B': 5, 'C': 1, 'D': 3})
     test_data = 'A'
-    test_neighbors = [['B', 'C', 'D']]
-    test_wieghts = {'B': 5, 'C': 1, 'D': 3}
+    test_neighbors = ['B', 'C', 'D']
+    test_weights = {'B': 5, 'C': 1, 'D': 3}
     assert test_vertex.data == test_data
     assert test_vertex.neighbors == test_neighbors
     assert test_vertex.weights == test_weights
+
+def test_wieghted_graph_exists():
+    """Test if graph is created."""
+    from weighted_graph import WGraph
+    wgraph = WGraph()
+    assert wgraph
+
+
+def test_weighted_graph_add_one_vertex():
+    from weighted_graph import WGraph
+    wgraph = WGraph()
+    wgraph.add_vertex()
+'''
--- weighted_graph.py
+++ weighted_graph.py
@@ -2,8 +2,52 @@
 
 
 class Vertex(object):
-    """Vertex component of a graph."""
-    def __init__ (self, data=None, neighbors=None, weights=None):
-        self.data = data
-        self.neighbors = neighbors
-        self.weights = weights
+    """Class for Vertex creation."""
+
+    def __init__(self, vertex=None):
+        """Let vertex be: { "Node": {"N1" : i, "Nk" : ik} }."""
+        self.node = None
+        self.vertex = vertex
+        self.neighbors = []
+
+    def get_vertex(self):
+        """Return primary node of a vertex."""
+        return list(self.vertex.keys())[0]
+
+    def update_neighbors(self):
+        """Return all neighbors of a given vertex node."""
+
+        for i in self.vertex:
+            for j in self.vertex[i]:
+                self.neighbors.append(j)
+
+    def get_weight(self, neighbor):
+        """Get weight from one neighbor."""
+
+        weight = 0
+        for i in self.vertex:
+            weights = self.vertex[i]
+            weight = weights[neighbor]
+
+        return weight
+
+    #add connection
+    def add_connection(self, new_neighbor):
+        self.neighbors.append(new_neighbor)
+
+
+'''
+class WGraph(object):
+    def __init__ (self, vertices=None):
+        self.vertices = []
+
+
+    def return_vertices(self):
+        for i in vertices:
+            print(i)
+
+    def add_vertex(self, vertex_to_add):
+        """Add new vertex."""
+        vertex = Vertex(vertex_to_add)
+        self.vertices.append(vertex)
+'''
