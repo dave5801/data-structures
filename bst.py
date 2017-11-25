f2012d0638c8ddb5681ba700c7ccd9d1f0410656
David Franklin <dave5801@gmail.com>
2017-11-25 13:50:48 -0800

    added get depth function

--- bst.py
+++ bst.py
@@ -16,8 +16,8 @@ class Tree(object):
 
     def __init__(self):
         """Constructor."""
-        self.size = 0
         self.root = None
+        self.nodes = []
 
     def size(self):
         """Return size of Tree."""
@@ -28,7 +28,6 @@ class Tree(object):
 
         if self.root is None:
             self.root = TreeNode(val)
-            self.size += 1
             return
 
         current = self.root
@@ -39,40 +38,70 @@ class Tree(object):
                     current = current.left
                 else:
                     current.left = TreeNode(val)
-                    self.size += 1
+                    self.nodes.append(val)
                     break
             if val > current.data:
                 if current.right:
                     current = current.right
                 else:
                     current.right = TreeNode(val)
-                    self.size += 1
+                    self.nodes.append(val)
                     break
+                '''
+                '''
+    def contains(self, val):
+        """Search for value in BST"""
+        if val in self.nodes:
+            return True
+        else:
+            return False
 
     def search(self, val):
-        """Search for value in BST"""
+     
         if self.root is None:
             return
 
         current = self.root
 
         while current:
+
+            if current.data == val:
+                return current
+
             if val < current.data:
-                if current.left:
-                    current = current.left
+                current = current.left
             elif val > current.data:
-                if current.right:
-                    current = current.right
-            elif current.data == val:
-                return current.data
-            else:
-                return None
-           
+                current = current.right
+
+        return None
+        
+
+    def get_depth(self, node=None):
+        """Get depth of tree"""
+
+        if node is None:
+            return 0
+
+        ld = self.get_depth(node.left)
+        rd = self.get_depth(node.right)
+
+        if node.left and node.left.data > node.right.data:
+            return ld + 1
+        else:
+            return rd + 1
 
 
 if __name__ == '__main__':
     t = Tree()
-    t.insert(10)
     t.insert(5)
-    t.insert(8)
-    print(t.search(8))
+    t.insert(2)
+    t.insert(3)
+    t.insert(1)
+    t.insert(7)
+    t.insert(10)
+    t.insert(20)
+    t.insert(30)
+
+    #print(t.search(5).data)
+    print(t.get_depth(t.root))
+    
