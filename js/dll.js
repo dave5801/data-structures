cae9a4c2167f82226fd0be9752d9bc6801eb67c3
David Franklin <dave5801@gmail.com>
2017-12-02 22:00:14 -0800

    push and append methods for dll

---
+++ js/dll.js
@@ -0,0 +1,72 @@
+class Node{
+    //node class
+
+    constructor(data, next, prev){
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+class DLL{
+
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    len(){
+        return this.size;
+    }
+
+    push(val){
+       //add new node to front of list.
+
+       var new_node = new Node(val);
+
+       if(!this.head){
+            this.head = new_node;
+            this.tail = this.head;
+            this.size += 1;
+            return;
+        }else{
+            this.head.prev = new_node;
+            new_node.next = this.head;
+            this.head = new_node;
+            
+        }
+    }
+    append(val){
+        //add new node to end of list.
+        var new_node = new Node(val);
+
+        if(!this.head){
+            this.head = new Node(val);
+            this.tail = this.head;
+            this.size += 1;
+            return;
+        }else{
+            this.tail.next = new_node;
+            new_node.prev = this.tail;
+            this.tail = new_node;
+            
+        }
+
+
+    }
+    pop(){}
+    shift(){}
+    remove(val){}
+}
+
+dll = new DLL();
+dll.push(10)
+dll.push(5)
+dll.append(3)
+console.log(dll.head.data)
+//console.log(dll.head.next.data)
+console.log(dll.tail.data)
+console.log(dll.tail.prev.data)
+//console.log(dll.len())
+
