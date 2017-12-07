9d73bf0c45665dc708fcde8a6f2a7a8130c03b3f
David Franklin <dave5801@gmail.com>
2017-12-06 22:41:25 -0800

    new tests for bubble sort

--- sorting/sortings.py
+++ sorting/sortings.py
@@ -1,26 +1,27 @@
+"""Class for sorting algorithms."""
+
 
 class Sortings(object):
-    
+    """A general class for sorting algorithms."""
+
     def __init__(self, sort_list=None):
-        
-        if sort_list == None:
+        """Take in empty list."""
+        if sort_list is None:
             self.sort_list = []
         else:
             self.sort_list = sort_list
 
-
     def bubble_sort(self):
-        for i in range(len(self.sort_list)-1):
-            for j in range(len(self.sort_list)-1):
-                if self.sort_list[j] > self.sort_list[j+1]:
+        """Bubble sort."""
+        for i in range(len(self.sort_list) - 1):
+            for j in range(len(self.sort_list) - 1):
+                if self.sort_list[j] > self.sort_list[j + 1]:
                     temp = self.sort_list[j]
-                    self.sort_list[j] = self.sort_list[j+1]
-                    self.sort_list[j+1] = temp
+                    self.sort_list[j] = self.sort_list[j + 1]
+                    self.sort_list[j + 1] = temp
 
 
 if __name__ == '__main__':
-    #bubble_sort([5,4,3,2,1])
-    s = Sortings([5,4,3,2,1])
+    s = Sortings([5, 4, 3, 2, 1])
     tl = s.bubble_sort()
     print(s.sort_list)
- 
--- sorting/test_sort.py
+++ sorting/test_sort.py
@@ -1,15 +1,45 @@
-import random
+"""Test class for sorting algorithms."""
+
+from random import randint
 
 
 def test_bubble_test_none_list():
+    """Test none list."""
     from sortings import Sortings
     s = Sortings()
     s.bubble_sort()
     assert s.sort_list == []
 
+
 def test_empty_list():
-    """Test Create a Tree Node."""
+    """Bubble sort on empty list."""
     from sortings import Sortings
     s = Sortings([])
     s.bubble_sort()
     assert s.sort_list == []
+
+
+def test_list_one_element_bubble_sort():
+    """Bubble sort on one element."""
+    from sortings import Sortings
+    s = Sortings([1])
+    s.bubble_sort()
+    assert s.sort_list == [1]
+
+
+def test_simple_list_bubble_sort():
+    """Bubble sort on basic list."""
+    from sortings import Sortings
+    s = Sortings([5, 4, 3, 2, 1])
+    s.bubble_sort()
+    assert s.sort_list == [1, 2, 3, 4, 5]
+
+
+def test_sort_random_list():
+    """Test random list of integers."""
+    from sortings import Sortings
+    x = [randint(0,9) for p in range(0, 9)]
+    s = Sortings(x)
+    s.bubble_sort()
+    x.sort()
+    assert s.sort_list == x
