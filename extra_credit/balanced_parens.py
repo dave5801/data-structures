b7848eeee14eabe55b65c33319325d46ea51efad
David Franklin <dave5801@gmail.com>
2018-01-01 14:50:39 -0800

    implemented with stack

--- extra_credit/balanced_parens.py
+++ extra_credit/balanced_parens.py
@@ -1,30 +1,39 @@
 """Extra credit from CodeWars."""
+from stack import Stack
 
 
-def is_balanced(parens):
+def is_balanced(string):
     """Check if string of parenthesis is valid."""
-    count = 0
-    index = 0
+    """Reverse string and use stack, in accordance with assignment."""
+    rev = string[::-1]
+    parens = Stack()
 
-    while index < len(parens):
-        if parens[index] == '(':
+    for i in rev:
+        parens.push(i)
+
+    count = 0
+
+    for i in range(parens.__len__()):
+        p = parens.pop()
+
+        if p == '(':
             count += 1
-        elif parens[index] == ')':
+        elif p == ')':
             count -= 1
 
         if count < 0:
             break
 
-        index += 1
-
     if count == 0:
-        return True
+        return 'balanced'
+    elif count == 1:
+        return 'open'
     else:
-        return False
+        return 'broken'
 
 
 if __name__ == '__main__':
     """Main method."""
-    parens = "hi())("
+    parens = "())"
     balanced = is_balanced(parens)
     print(balanced)
