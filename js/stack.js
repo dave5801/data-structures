4e89733676b0004f4a12d1a4619f17b05f7f0626
David Franklin <dave5801@gmail.com>
2018-01-02 14:09:40 -0800

    starting tests for stack

--- js/stack.js
+++ js/stack.js
@@ -125,3 +125,5 @@ stack.push(5);
 stack.push(6);
 stack.push(7);
 console.log(stack.size());
+
+module.exports = Stack;
---
+++ js/stack.test.js
@@ -0,0 +1,46 @@
+/*Test class for Stack.*/
+
+/*requirements*/
+const Stack = require('./stack.js');
+
+/*Tests*/
+
+test('test create new stack', () => {
+    const s = new Stack();
+    expect(s);
+});
+
+/*
+test('test size of stack empty', () => {
+    const s = new Stack();
+    expect(s);
+});
+
+test('test size of stack', () => {
+    const s = new Stack();
+    expect(s);
+});
+
+test('test len of stack', () => {
+    const s = new Stack();
+    expect(s);
+});
+
+test('test push to stack', () => {
+    const s = new Stack();
+    expect(s);
+});
+
+test('test pop stack', () => {
+    const s = new Stack();
+    expect(s);
+})
+
+
+test('test pop empty stack', () => {
+    const s = new Stack();
+    expect(s);
+});
+
+
+*/
