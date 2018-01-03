f0b979c430f70b34513f930ae5d77c975f5da5f6
David Franklin <dave5801@gmail.com>
2018-01-03 15:23:07 -0800

    it works on one pass

--- sorting/radix_sort.py
+++ sorting/radix_sort.py
@@ -6,20 +6,25 @@ def radix_sort(lst):
 
     buckets = [[] for i in range(len(lst))]
     unsorted = {}
+    sorted_nums = []
 
     for i in range(len(buckets)):
         tmp = get_digit(lst[i], 1)
         buckets[i].append(tmp)
-        #print(tmp, lst[i])
         unsorted[lst[i]] = buckets[i]
 
-    print(unsorted)
+    unsorted = sort_values(unsorted)
+
+    for j in unsorted:
+        sorted_nums.append(j[0])
+    
+    print(sorted_nums)
 
 
 
 
-def sort_values(d):
-    return sorted(d.items(), key=operator.itemgetter(1))
+def sort_values(unsorted):
+    return sorted(unsorted.items(), key=operator.itemgetter(1))
 
 def get_digit(number, n):
     return number // 10**n % 10
