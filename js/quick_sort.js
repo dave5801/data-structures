b117e130868e27275d96d7168d4a160a0a284792
David Franklin <dave5801@gmail.com>
2018-01-04 16:18:00 -0800

    done with quicksort

--- js/quick_sort.js
+++ js/quick_sort.js
@@ -1,4 +1,4 @@
-//https://www.w3resource.com/javascript-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-1.php
+//quick sort method.
 
 var quick_sort = function(unsorted){
 
@@ -27,30 +27,3 @@ unsorted = [10,7,4,200,6,8,5,28]
 console.log(quick_sort(unsorted))
 
 module.exports = quick_sort;
-
-/*
-function quick_sort(unsorted){
-
-    if(unsorted.length <= 1){
-        return unsorted
-    }
-
-    let pivot = unsorted.pop()
-    let left = []
-    let right = []
-    let sorted = []
-
-    for(let i = 0; i < unsorted.length; i++){
-        if(unsorted[i] <= pivot){
-            left.push(unsorted[i])
-        }else if(unsorted[i] > pivot){
-            right.push(unsorted[i])
-        }
-    }
-    return sorted.concat(quick_sort(left), pivot, quick_sort(right));
-}
-
-unsorted = [10,7,4,200,6,8,5,28]
-
-
-console.log(quick_sort(unsorted))*/
