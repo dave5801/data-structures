1c2ed71ef2a71d9b9723f8dee9edbe32145df8f6
David Franklin <dave5801@gmail.com>
2018-02-21 12:50:13 -0800

    last save on trie for the day

--- trie.py
+++ trie.py
@@ -1,14 +1,11 @@
 class Node(object):
-    
+
     def __init__(self, data=None, children=None):
         self.node = {}
         self.data = data
         self.children = children
-        
-        self.node[data] = children 
 
-    def get_node_info(self):
-        return "Current Node: " + self.data + " Children: " + self.children
+        self.node[data] = children 
 
 
 class Trie(object):
@@ -19,12 +16,13 @@ class Trie(object):
 
     def insert_into_trie(self, string_to_insert):
         print(string_to_insert[0])
+        print(self.immediate_child_node_keys)
         if string_to_insert[0] in self.immediate_child_node_keys:
             print("already in the trie")
         else:
             for letter in range(len(string_to_insert)):
                 new_node = Node(string_to_insert[letter],string_to_insert[letter+1:len(string_to_insert)])
-                self.immediate_child_node_keys.append(new_node)
+                self.immediate_child_node_keys.append(new_node.data)
 
 
 
@@ -49,10 +47,7 @@ if __name__ == '__main__':
     print(trie.insert_into_trie(x))
 
     keys = trie.immediate_child_node_keys
-    print("Get Nodes form Trie")
-    for i in keys:
-        print(i.get_node_info())
-    print()
+
 
     print("Second Test Case")    
     print(trie.insert_into_trie(y))    
