717499217631cf2ae51e01421c0621756d0fd632
David Franklin <dave5801@gmail.com>
2018-02-21 11:17:38 -0800

    setting up Node and Trie methods

--- trie.py
+++ trie.py
@@ -1,11 +1,22 @@
 class Node(object):
-    def __init(self):
-        return " "
+    def __init(self, data, children):
+        self.data = data
+        self.children = children
 
 
 class Trie(object):
-    def __int(self):
-        return "Trie class"
+
+    immediate_child_node_keys = []
+
+    def __int(self,string_to_insert):
+        self.string_to_insert = string_to_insert
+    
+
+    def insert(self):
+        return ""
+
+    def search(self):
+        return ""
 
 
 if __name__ == '__main__':
