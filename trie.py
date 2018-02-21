723b16e07b63dafeeffb65d0cafe2df350fd84a3
David Franklin <dave5801@gmail.com>
2018-02-21 11:09:10 -0800

    creating trie branch

---
+++ trie.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init(self):
+        return " "
+
+
+class Trie(object):
+    def __int(self):
+        return "Trie class"
+
+
+if __name__ == '__main__':
+    x = "cat"
+    y = "car"
+    z = "house"
+
+    trie = Trie()
+    print(trie)
